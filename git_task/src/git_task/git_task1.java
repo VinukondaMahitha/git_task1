6682dc2bf17542b22f846c5c489622db01868635
Lenovo <Lenovo@DESKTOP-ESKVQ4N>
2020-02-07 21:21:13 +0530

    volume of box

---
+++ git_task/src/git_task/git_task1.java
@@ -0,0 +1,34 @@
+package git_task;
+
+import java.util.*;
+	import java.io.*;
+	class Box
+	{
+	int p, q, r;
+	void init(int x,int y, int z)
+	{
+	this.p=x;
+	this.q=y;
+	this.r=z;
+	}
+	int volume()
+	{
+	int vol=(p*q*r);
+	return vol;
+	}
+	}
+	public class git_task1
+	{
+	public static void main(String args[])
+	{
+	Scanner sc=new Scanner(System.in);
+	Box b=new Box();
+	System.out.println("enter length,width,height:");
+	int p=sc.nextInt();
+	int q=sc.nextInt();
+	int r=sc.nextInt();
+	b.init(p,q,r);
+	int v=b.volume();
+	System.out.println("vol : "+v);
+	}
+	}
